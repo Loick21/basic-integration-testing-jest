bfbd8dc720255f54eecb5d2942898b1e3842f461
loick21 <loicnzenang@gmail.com>
2022-02-11 13:24:14 +0100

    deleteTodo impl and test

--- src/todos.js
+++ src/todos.js
@@ -2,6 +2,7 @@ const Router = require("koa-router")
 
 const router = Router({ prefix: "/todos" })
 const { getDB } = require("./database")
+const {ObjectId} = require("mongodb");
 
 router
     .get("/", listTodos)
@@ -20,12 +21,17 @@ async function listTodos (ctx) {
 }
 
 async function createTodo (ctx) {
-    const title = ctx.request.body.title
+    const title = ctx.request.body.title.trim();
 
     if (title === null || title === undefined) {
         ctx.status = 422
         ctx.body = { errorMsg: "Missing parameter 'title'" }
-    } else {
+    }
+    else if(title === ""){
+        ctx.status = 422
+        ctx.body = { errorMsg: "invalid'title'" }
+    }
+    else {
         const result = await getDB().collection("todos").insertOne({
             title,
             completed: false,
@@ -38,11 +44,45 @@ async function createTodo (ctx) {
 }
 
 async function deleteTodo (ctx) {
-    // TODO
+    const id = ctx.request.params.id;
+    const todo = await getDB()
+        .collection("todos")
+        .findOne({_id: ObjectId(id)});
+
+    if(!todo)  {
+        ctx.status = 404;
+        ctx.body = {errorMsg: `${id} not found`}
+        return;
+    }
+
+    try{
+        await getDB()
+            .collection("todos")
+            .deleteOne({ _id: ObjectId(id) });
+        ctx.status = 204;
+        ctx.body = { message: `${id} deleted` }
+    }
+    catch (error){
+        console.log(error);
+    }
 }
 
 async function updateTodo (ctx) {
     // TODO
+    const id = ctx.request.params.id;
+
+    try{
+        await getDB()
+            .collection("todos")
+            .up
+        ctx.status = 200;
+        ctx.body = { message: "updated"};
+    }
+    catch (error){
+        if(error instanceof BSONTypeError)
+        ctx.status = 400;
+        ctx.body = { message: "not updated"};
+    }
 }
 
 module.exports = router
--- test/todos.test.js
+++ test/todos.test.js
@@ -1,9 +1,9 @@
-const { ObjectId } = require("mongodb")
+const {ObjectId} = require("mongodb")
 const request = require("supertest")
 const app = require("../src/app")
-const { connectToDB, closeConnection, getDB } = require("../src/database")
+const {connectToDB, closeConnection, getDB} = require("../src/database")
 
-const baseUrl = "/todos"
+const baseUrl = "/todos";
 
 beforeAll(async () => {
     const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017'
@@ -15,8 +15,20 @@ beforeAll(async () => {
 beforeEach(async () => {
     const db = getDB();
     await db.createCollection("todos");
+/*    const data = [
+        {
+            _id: 1,
+            title: "title",
+            completed: false,
+            createdAt: "2021",
+            updatedAt: "2022"
+        }
+    ];
+   db.collection("todos").insertMany(data, function (err, res) {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + res.insertedCount);
+    });*/
 });
-
 
 afterEach(async () => {
     const db = getDB();
@@ -29,17 +41,75 @@ afterAll(async () => {
 
 describe("GET /todos", () => {
     test("should respond with a 200 status code", async () => {
-        // TODO
-        fail("Not yet implemented")
-    })
+        const response = await request(app.callback()).get(baseUrl);
+        expect(response.statusCode).toBe(200);
+    });
 
-    test("should respond with JSON", async () => {
-        // TODO
-        fail("Not yet implemented")
-    })
+    test("should respond with LIST OF JSON", async () => {
+        const response = await request(app.callback()).get(baseUrl);
+        expect(response.type).toBe("application/json");
+    });
 
     test("should respond with list of existing todos", async () => {
-        // TODO
-        fail("Not yet implemented")
-    })
-})
+        const response = await request(app.callback()).get(baseUrl);
+        const data = [{
+            _id: 1,
+            title: "title",
+            completed: false,
+            createdAt: "2021",
+            updatedAt: "2022"
+        }];
+        expect(response.body).toStrictEqual(data);
+    });
+});
+
+// POST todos
+
+describe("POST /todos", () => {
+    test("should respond with a 200 status code", async () => {
+        const data = { title: "titleTwo" };
+        const response = await request(app.callback())
+            .post(baseUrl)
+            .send(data);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test("should respond with a 422 status code without title", async () => {
+        const data = { };
+        const response = await request(app.callback())
+            .post(baseUrl)
+            .send(data);
+        expect(response.statusCode).toBe(422);
+        expect(response.body).toBe({ errorMsg: "Missing parameter 'title'" });
+        console.log(response.body)
+    });
+
+    test("should respond with a 422 status code with title blank", async () => {
+        const data = { title: "    " };
+        const response = await request(app.callback())
+            .post(baseUrl)
+            .send(data);
+        expect(response.statusCode).toBe(422);
+    });
+
+});
+
+describe("DELETE todos", () => {
+
+    test("should delete ", async () => {
+        const data = { title: "titleTwo" };
+        await request(app.callback())
+            .post(baseUrl)
+            .send(data);
+
+        const create_response = await request(app.callback()).get(baseUrl);
+        const receive_data = create_response.body.shift();
+        const delete_response = await request(app.callback())
+            .delete(`${baseUrl}/${receive_data._id}`);
+
+        console.log(delete_response.body)
+        expect(delete_response.statusCode).toBe(204);
+        expect(delete_response.body).toBe({message:"deleted"});
+    });
+
+});

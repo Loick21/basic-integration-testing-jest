ef84b3aaa0674c1dd569f5d2005b55c8dc89d8b7
loick21 <loicnzenang@gmail.com>
2022-02-13 21:04:40 +0100

    refactoring of all the tests

--- src/todos.js
+++ src/todos.js
@@ -1,7 +1,7 @@
 const Router = require("koa-router")
 
-const router = Router({ prefix: "/todos" })
-const { getDB } = require("./database")
+const router = Router({prefix: "/todos"})
+const {getDB} = require("./database")
 const {ObjectId} = require("mongodb");
 
 router
@@ -10,26 +10,28 @@ router
     .put("/:id", updateTodo)
     .del("/:id", deleteTodo)
 
-async function listTodos (ctx) {
+async function listTodos(ctx) {
     const todos = await getDB()
         .collection("todos")
         .find({})
-        .sort({ _id: 1 })
+        .sort({_id: 1})
         .toArray()
 
     ctx.body = todos
 }
 
-async function createTodo (ctx) {
-    const title = ctx.request.body.title.trim();
+async function createTodo(ctx) {
+    let title = ctx.request.body.title;
 
     if (title === null || title === undefined) {
         ctx.status = 422
-        ctx.body = { errorMsg: "Missing parameter 'title'" }
+        ctx.body = {errorMsg: "Missing parameter 'title'"}
+        return;
     }
-    else if(title === ""){
+    title = title.trim();
+    if (title === "") {
         ctx.status = 422
-        ctx.body = { errorMsg: "invalid'title'" }
+        ctx.body = {errorMsg: "invalid'title'"}
     }
     else {
         const result = await getDB().collection("todos").insertOne({
@@ -43,46 +45,58 @@ async function createTodo (ctx) {
     }
 }
 
-async function deleteTodo (ctx) {
+async function deleteTodo(ctx) {
     const id = ctx.request.params.id;
+    if(!ObjectId.isValid(id)) {
+        ctx.status = 400;
+        ctx.body = {errorMsg : "id not valid"};
+        return;
+    }
     const todo = await getDB()
         .collection("todos")
         .findOne({_id: ObjectId(id)});
 
-    if(!todo)  {
+    if (!todo) {
         ctx.status = 404;
         ctx.body = {errorMsg: `${id} not found`}
         return;
     }
 
-    try{
+    try {
         await getDB()
             .collection("todos")
-            .deleteOne({ _id: ObjectId(id) });
+            .deleteOne({_id: ObjectId(id)});
         ctx.status = 204;
-        ctx.body = { message: `${id} deleted` }
-    }
-    catch (error){
-        console.log(error);
-    }
+    } catch (error) {}
 }
 
-async function updateTodo (ctx) {
-    // TODO
+async function updateTodo(ctx) {
     const id = ctx.request.params.id;
+    if(!ObjectId.isValid(id)) {
+        ctx.status = 400;
+        ctx.body = {errorMsg : "id not valid"};
+        return;
+    }
+    const body = ctx.request.body;
+    const todo = await getDB()
+        .collection("todos")
+        .findOne({_id: ObjectId(id)});
 
-    try{
+    if (!todo) {
+        ctx.status = 404;
+        ctx.body = {errorMsg: `${id} not found`}
+        return;
+    }
+    try {
         await getDB()
             .collection("todos")
-            .up
+            .updateOne(
+                {_id: id},
+                { $set: body}
+            );
         ctx.status = 200;
-        ctx.body = { message: "updated"};
-    }
-    catch (error){
-        if(error instanceof BSONTypeError)
-        ctx.status = 400;
-        ctx.body = { message: "not updated"};
-    }
+        ctx.body = {message: "updated"};
+    } catch (error) {}
 }
 
 module.exports = router
--- test/todos.test.js
+++ test/todos.test.js
@@ -15,19 +15,6 @@ beforeAll(async () => {
 beforeEach(async () => {
     const db = getDB();
     await db.createCollection("todos");
-/*    const data = [
-        {
-            _id: 1,
-            title: "title",
-            completed: false,
-            createdAt: "2021",
-            updatedAt: "2022"
-        }
-    ];
-   db.collection("todos").insertMany(data, function (err, res) {
-        if (err) throw err;
-        console.log("Number of documents inserted: " + res.insertedCount);
-    });*/
 });
 
 afterEach(async () => {
@@ -51,15 +38,19 @@ describe("GET /todos", () => {
     });
 
     test("should respond with list of existing todos", async () => {
-        const response = await request(app.callback()).get(baseUrl);
-        const data = [{
-            _id: 1,
+        const todo = {
             title: "title",
             completed: false,
-            createdAt: "2021",
-            updatedAt: "2022"
-        }];
-        expect(response.body).toStrictEqual(data);
+        };
+        const create_response = await request(app.callback())
+            .post(baseUrl)
+            .send(todo);
+
+        const get_response = await request(app.callback()).get(baseUrl);
+
+        expect(get_response.body.length).toStrictEqual(1);
+        expect(get_response.body[0].title).toStrictEqual("title");
+        expect(get_response.body[0].completed).toStrictEqual(false);
     });
 });
 
@@ -67,25 +58,25 @@ describe("GET /todos", () => {
 
 describe("POST /todos", () => {
     test("should respond with a 200 status code", async () => {
-        const data = { title: "titleTwo" };
+        const data = {title: "titleTwo"};
         const response = await request(app.callback())
             .post(baseUrl)
             .send(data);
         expect(response.statusCode).toBe(200);
+        expect(ObjectId.isValid(response.body.id)).toBe(true);
     });
 
     test("should respond with a 422 status code without title", async () => {
-        const data = { };
+        const data = {};
         const response = await request(app.callback())
             .post(baseUrl)
             .send(data);
         expect(response.statusCode).toBe(422);
-        expect(response.body).toBe({ errorMsg: "Missing parameter 'title'" });
-        console.log(response.body)
+        expect(response.body.errorMsg).toBe("Missing parameter 'title'");
     });
 
-    test("should respond with a 422 status code with title blank", async () => {
-        const data = { title: "    " };
+    test("should respond with a 422 status code with a blank title ", async () => {
+        const data = {title: "    "};
         const response = await request(app.callback())
             .post(baseUrl)
             .send(data);
@@ -94,22 +85,78 @@ describe("POST /todos", () => {
 
 });
 
-describe("DELETE todos", () => {
+describe("DELETE /todos/:id", () => {
 
-    test("should delete ", async () => {
-        const data = { title: "titleTwo" };
-        await request(app.callback())
+    test("should delete and return 204 ", async () => {
+        const data = {title: "titleTwo"};
+        const create_response = await request(app.callback())
             .post(baseUrl)
             .send(data);
 
-        const create_response = await request(app.callback()).get(baseUrl);
-        const receive_data = create_response.body.shift();
-        const delete_response = await request(app.callback())
-            .delete(`${baseUrl}/${receive_data._id}`);
+        const id = JSON.parse(create_response.text).id;
 
-        console.log(delete_response.body)
+        const delete_response = await request(app.callback())
+            .delete(`${baseUrl}/${id}`);
         expect(delete_response.statusCode).toBe(204);
-        expect(delete_response.body).toBe({message:"deleted"});
     });
 
+    test("invalid id should return 400  ", async () => {
+        const id = 1;
+        const delete_response = await request(app.callback())
+            .delete(`${baseUrl}/${id}`);
+        expect(delete_response.statusCode).toBe(400);
+        expect(delete_response.body.errorMsg).toBe("id not valid");
+    });
+    test("not found todo for id should  return 404  ", async () => {
+        const id = "62095ba420d5d27fa5a3bdfb";
+        const delete_response = await request(app.callback())
+            .put(`${baseUrl}/${id}`);
+        expect(delete_response.statusCode).toBe(404);
+        expect(delete_response.body.errorMsg).toBe(`${id} not found`);
+    });
+
+});
+
+describe("PUT todos/:id", () => {
+
+    test("should update and return 200", async () => {
+        const data = {title: "titleTwo"};
+        const response = await request(app.callback())
+            .post(baseUrl)
+            .send(data);
+
+        const id = JSON.parse(response.text).id;
+        console.log(id)
+        const body = {
+            title: "titleOne",
+        };
+        const update_response = await request(app.callback())
+            .put(`${baseUrl}/${id}`)
+            .send(body);
+        expect(update_response.statusCode).toBe(200);
+        expect(update_response.body.message).toBe("updated");
+    });
+
+    test("invalid id should return 400  ", async () => {
+        const id = 1;
+        const body = {
+            title: "titleOne",
+        };
+        const update_response = await request(app.callback())
+            .put(`${baseUrl}/${id}`)
+            .send(body);
+        expect(update_response.statusCode).toBe(400);
+        expect(update_response.body.errorMsg).toBe("id not valid");
+    });
+    test("not found todo for id should  return 404  ", async () => {
+        const id = "62095ba420d5d27fa5a3bdfb";
+        const body = {
+            title: "titleOne",
+        };
+        const update_response = await request(app.callback())
+            .put(`${baseUrl}/${id}`)
+            .send(body);
+        expect(update_response.statusCode).toBe(404);
+        expect(update_response.body.errorMsg).toBe(`${id} not found`);
+    });
 });
